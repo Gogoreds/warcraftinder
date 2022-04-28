8af058b442dc06136d4a3992988deb65ac0907f4
Mohammad Arefi <m-arefi@live.com>
2022-04-28 23:10:29 +0200

    Ready to present

--- warcraftinder/src/components/Header.js
+++ warcraftinder/src/components/Header.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Logo from "./Logo";
+import React from 'react';
+import Logo from './Logo';
 
-const Header = () => {
-    <header>
-        <div className="fl">
-            <button type="button">
-                <img src="/warcraftinder/public/images/misc/user.png" alt="User Settings" />
-            </button>
-        </div>
+const Header = () => (
+  <header>
+    <div className="fl">
+      <button type="button">
+        <img src="/images/misc/user.png" alt="User Settings" />
+      </button>
+    </div>
 
-        <div className="fl">
-            <Logo />
-        </div>
+    <div className="fl">
+      <Logo />
+    </div>
 
-        <div className="fl">
-            <button type="button">
-                <img src="/warcraftinder/public/images/misc/messages.png" alt="View Messages" />
-            </button>
-        </div>
-    </header>
-};
+    <div className="fl">
+      <button type="button">
+        <img src="/images/misc/messages.png" alt="View Messages" />
+      </button>
+    </div>
+  </header>
+);
 
 export default Header;
