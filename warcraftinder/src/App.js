a6ed42fb0dceb888b669f0cd22f81ca4fbc4f77f
Mohammad Arefi <m-arefi@live.com>
2022-04-28 21:27:26 +0200

    Main actions defined

--- warcraftinder/src/App.js
+++ warcraftinder/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Header';
+import Person from './Person';
+import Lonely from './Lonely';
+import data from './data.json';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = useState([]);
+  const [superLikedUsers, setSuperLikedUsers] = useState([]);
+  const [dislikedUsers, setDislikedUsers] = useState([]);
+  const activeUser = 0;
+
+  switch (action) {
+    case 'ADD_TO_LIKED_USERS':
+      break;
+    case 'ADD_TO_DISLIKED_USERS':
+      break;
+    case 'ADD_TO_SUPER_LIKED_USERS':
+      break;
+    default:
+      return people;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
